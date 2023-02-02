fcba6964ade6b3585163ead64d6cdc1f72ea6463
Blake Whitlock <blake_whitlock_2026@outlook.com>
2023-02-01 19:29:25 -0500

    2/1

--- chapter3/functions.js
+++ chapter3/functions.js
@@ -18,7 +18,20 @@ console.log(power(2,10));
 let launchMissiles = function() {
         missileSystem.launch("now");
 };
-if (safeMode) {
+if (true) {
     launchMissiles = function() {/* do nothing */};
 }
-    
+//growingFunction
+function printFarmInventory(cows, chickens) {
+    let cowString = String(cows);
+    while (cowString.length < 3) {
+        cowString = "0" + cowString;
+    }
+    console.log(`${cowString} Cows`);
+    let chickenString = String(chickens);
+    while (chickenString.length < 3) {
+        chickenString = "0" + chickenString;
+    }
+    console.log(`${chickenString} Chickens`);
+    }
+    printFarmInventory(7, 11);
